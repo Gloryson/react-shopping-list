7a57eae1ebb55a41922f8af64fd279e3a7be7563
Vlad Bolkunov <gloryson@mail.ru>
2022-10-04 19:59:26 +0300

    feat: new game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.scss';
+import Item from './Item';
+import db from './db';
 
 
 
 
 function App() {
-  return <div>GGGGGG</div>
+
+  
+
+  return (
+    <div className='container'>
+      <Item db = {db}/>
+    </div>
+  )
 }
 
 export default App;
