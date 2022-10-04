ff22b01a0727c24563cbd2d9d8c7225d4737ca2d
Vlad Bolkunov <gloryson@mail.ru>
2022-10-04 19:30:20 +0300

    feat: new game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { useState } from 'react';
 import './App.scss';
-import App2 from './App2';
-import array from './db/db';
 
 
 
 
 function App() {
-  const [arr, setArr] = useState(array);
-
-  function gg () {
-    setArr(arr.map(e => ++e))
-  }
-
-  return (
-    <>
-    <button onClick={() => alert(arr)}>111</button>
-    <div>222</div>
-      <App2 gg = {gg} arr = {arr}/>
-    </>
-  );
+  
 }
 
 export default App;
